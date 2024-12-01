e6c841815f26ab94d83929609c4b5fd379479f27
JR-Fire <jrfire@gmail.com>
2024-12-01 12:40:21 +0200

    run tests fix deno v2

--- adventofcode/2024/dec-01.ts
+++ adventofcode/2024/dec-01.ts
@@ -20,23 +20,22 @@ export function listDistance(input: number[][]): number {
 }
 
 export function listSimilarity(input: number[][]): number {
-    
     const l1: number[] = [];
     const m2 = new Map<number, number>();
     const l = input.length;
     for (let i = 0; i < l; i++) {
         const location1 = input[i][0];
         l1.push(location1);
-        
+
         const location2 = input[i][1];
         const count = m2.get(location2) || 0;
         m2.set(location2, count + 1);
     }
-    
+
     let distance = 0;
     for (let i = 0; i < l; i++) {
-        const location = l1[i]
-        distance += (location * (m2.get(location) || 0));
+        const location = l1[i];
+        distance += location * (m2.get(location) || 0);
     }
 
     return distance;
