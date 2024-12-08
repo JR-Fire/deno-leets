909379c2b5314575912140768c47cc384a34fbb9
JR-Fire <jrfire@gmail.com>
2024-12-09 00:22:22 +0200

    day08

---
+++ adventofcode/2024/test/dec-08.test.ts
@@ -0,0 +1,48 @@
+import { expect } from "jsr:@std/expect";
+import { antinodes, harmonicAntinodes } from "../dec-08.ts";
+
+const { test } = Deno;
+
+
+test("Simple antinodes", () => {
+    expect(antinodes(simpleData)).toBe(3);
+});
+test("Sample antinodes", () => {
+    expect(antinodes(smapleData)).toBe(14);
+});
+
+test("Simple harmonized antinodes", () => {
+    expect(harmonicAntinodes(simpleData)).toBe(9);
+});
+
+test("Sample harmonized antinodes", () => {
+    expect(harmonicAntinodes(smapleData)).toBe(34);
+});
+
+const simpleData = [
+'T.........',
+'...T......',
+'.T........',
+'..........',
+'..........',
+'..........',
+'..........',
+'..........',
+'..........',
+'..........',
+].map(r => r.split(''));
+
+const smapleData = [
+    '............',
+    '........0...',
+    '.....0......',
+    '.......0....',
+    '....0.......',
+    '......A.....',
+    '............',
+    '............',
+    '........A...',
+    '.........A..',
+    '............',
+    '............',
+].map(r => r.split(''));
